67f82b06075c2b9161053ae2671da56166c1a491
Sugith Pietech <sugith@pietechsolution.com>
2026-01-02 15:29:38 +0530

    chore: Update `analyzer_plugin` dependency, add pubspec metadata, and simplify plugin starter imports.

--- packages/riverpod_lint/tools/analyzer_plugin/bin/plugin.dart
+++ packages/riverpod_lint/tools/analyzer_plugin/bin/plugin.dart
@@ -1,8 +1,7 @@
 import 'dart:isolate';
 
-import 'package:analysis_server_plugin/edit/server_plugin_starter.dart';
-import 'package:analyzer_plugin/starter.dart';
 import 'package:riverpod_lint/main.dart' as plugin;
+import 'package:analysis_server_plugin/starter.dart';
 
 void main(List<String> args, SendPort sendPort) {
   ServerPluginStarter(plugin.plugin).start(sendPort);
